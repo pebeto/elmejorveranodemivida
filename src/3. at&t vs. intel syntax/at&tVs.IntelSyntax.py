379aa0fa4c44a4a62d9e9885cc41a08831ed1648
pebeto <pebeto@users.noreply.github.com>
2019-01-26 15:32:00 -0500

    edits

---
+++ src/3. at&t vs. intel syntax/at&tVs.IntelSyntax.py
@@ -0,0 +1,4 @@
+v = ['¿Por qué no puedo ser mejor', 'si he hecho todo por ti?', '¿Dime qué es lo que hice mal?', 'No quiero otra oportunidad,', 'solo suponer que todo está bien.']
+
+for i in v:
+    print(i)
