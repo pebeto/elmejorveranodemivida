a33bc3bfd1862c2e955cf115a67ece194145e88a
pebeto <28990958+pebeto@users.noreply.github.com>
2022-04-30 00:06:36 -0500

    Updates

---
+++ precache-manifest.6db94eff8c2b665afd34244056cf357e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "797fa516d27140cfb2b5",
+    "url": "/elmejorveranodemivida/static/css/main.9bd7e6c1.chunk.css"
+  },
+  {
+    "revision": "797fa516d27140cfb2b5",
+    "url": "/elmejorveranodemivida/static/js/main.797fa516.chunk.js"
+  },
+  {
+    "revision": "a5df2eb8527f3be4374b",
+    "url": "/elmejorveranodemivida/static/css/1.977466e3.chunk.css"
+  },
+  {
+    "revision": "a5df2eb8527f3be4374b",
+    "url": "/elmejorveranodemivida/static/js/1.a5df2eb8.chunk.js"
+  },
+  {
+    "revision": "eb5b71e6a5e5025275d4",
+    "url": "/elmejorveranodemivida/static/js/runtime~main.eb5b71e6.js"
+  },
+  {
+    "revision": "edb4751678c418895219f4ae1d3cf586",
+    "url": "/elmejorveranodemivida/static/media/EMVDMV__ISOTIPO.edb47516.webp"
+  },
+  {
+    "revision": "8802a3bc9fe0cb807436fe1f13db3898",
+    "url": "/elmejorveranodemivida/static/media/LeOsler_Rough-Light.8802a3bc.ttf"
+  },
+  {
+    "revision": "c85b317cbd2fb88989f6ada7dc637584",
+    "url": "/elmejorveranodemivida/index.html"
+  }
+];
